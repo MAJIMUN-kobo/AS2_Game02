924d12b769d0faf07eb0110fbbef474cf1a28b49
NUCBOX_M5PLUS\ooshiro <yanbarumajimun@ryukyugame.page>
2025-12-08 13:04:44 +0900

    ・インゲームシーン管理用クラス（GameManager.cs）の追加

---
+++ Assets/Project/Management/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class GameManager : SingletonMonoBehaviour<GameManager>
+{
+    /*
+    public Player player { get; private set; }
+    public Enemy[] enemies { get; private set; }
+    public Item[] items { get; private set; }
+
+    public void Initialize()
+    {
+        player = FindAnyObjectByType<Player>();
+    }
+    */
+}
