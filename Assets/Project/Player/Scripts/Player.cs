14764e66b19ffa93056e7e0aa1e7de7ed5eb04db
OREDESU <asosora2357@gmail.com>
2025-12-08 10:53:29 +0900

    Playerの移動（カメラ無・ただ動くだけ）

---
+++ Assets/Project/Player/Scripts/Player.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField, Header("à⁄ìÆê›íË")]
+    private float _moveSpeed;
+    private Vector3 _moveVelocity;
+
+    void Start()
+    {
+        _moveSpeed = 3;
+    }
+
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
+
+        _moveVelocity = new Vector3(inputX,0,inputZ);
+
+        transform.Translate(_moveVelocity * _moveSpeed * Time.deltaTime);
+
+    }
+}
