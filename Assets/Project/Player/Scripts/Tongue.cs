88e9fd48ccc99418142098b0c7fe671be83e8ecd
asosora2357@gmail.com <asosora2357@gmail.com>
2026-01-26 13:07:32 +0900

    当たり判定、アイテムの使用の流れ？まぁ進んだ

--- Assets/Project/Player/Scripts/Tongue.cs
+++ Assets/Project/Player/Scripts/Tongue.cs
@@ -6,6 +6,9 @@ public class Tongue : MonoBehaviour
     [SerializeField,Header("移動速度")]
     private float _speed = 10.0f;
 
+    [SerializeField, Header("rayの長さ")]
+    private float _rayDistance;
+
     // == private変数
     private Player _player;
 
@@ -16,15 +19,29 @@ public class Tongue : MonoBehaviour
 
     void Update()
     {
+        // rayを飛ばす
+        Vector3 rayPosition = transform.position;
+        bool rayForward = Physics.Raycast(rayPosition, Vector3.forward, _rayDistance);
+        bool rayBack = Physics.Raycast(rayPosition, Vector3.back, _rayDistance);
+        bool rayRight = Physics.Raycast(rayPosition, Vector3.right, _rayDistance);
+        bool rayLeft = Physics.Raycast(rayPosition, Vector3.left, _rayDistance);
+        bool rayDown = Physics.Raycast(rayPosition, Vector3.down, _rayDistance);
+
+        // rayの可視化
+        Debug.DrawRay(rayPosition, Vector3.forward * _rayDistance, Color.red);
+        Debug.DrawRay(rayPosition, Vector3.back * _rayDistance, Color.red);
+        Debug.DrawRay(rayPosition, Vector3.right * _rayDistance, Color.red);
+        Debug.DrawRay(rayPosition, Vector3.left * _rayDistance, Color.red);
+        Debug.DrawRay(rayPosition, Vector3.down * _rayDistance, Color.red);
+
+        if (rayForward || rayBack || rayRight || rayLeft || rayDown)
+        {
+            // テレポート関数実行
+            _player.Teleport(transform.position);
+            Destroy(gameObject);
+        }
+
         // 移動
         transform.Translate(Vector3.forward * _speed * Time.deltaTime);
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        // テレポート関数実行！
-        _player.Teleport(transform.position);
-        // 自分を消すぜ
-        Destroy(gameObject);
     }
 }
