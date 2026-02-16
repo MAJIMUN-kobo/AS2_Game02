6276e103d32da63b53e8cf3422b4074d5d7f2dbf
giraffe-lion999 <shanbentaiki5@gmail.com>
2026-02-16 10:20:42 +0900

    Canvasの修正

---
+++ Assets/Project/System/Scripts/CanvasManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject pauseMenu;
+
+
+    public string titleSceneName = "TitleScene";
+
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseMenu.activeSelf)
+            {
+                pauseMenu.SetActive(false);
+            }
+            else
+            {
+                pauseMenu.SetActive(true);
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    public void BackToTitle()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(titleSceneName);
+    }
+}
